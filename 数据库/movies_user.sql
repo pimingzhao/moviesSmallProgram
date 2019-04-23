a5325323d845250a1fa4933734c868e2041475d6
xiaoPi <1105133421@qq.com>
2019-04-23 11:58:44 +0800

    as

---
+++ 数据库/movies_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('1', '鹿晗', 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1309889552,399749004&fm=58&bpow=960&bpoh=1394');
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('2', '陈伟霆', 'https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D500/sign=e52171c617178a82ca3c7fa0c603737f/622762d0f703918fc7ebb6c0583d269759eec486.jpg');
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('3', '无极', 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3718183995,4251190823&fm=58&bpow=178&bpoh=220');
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('4', '疾风剑豪', 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2530381103,413657287&fm=58');
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('5', '赵丽颖', 'https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D500/sign=5411499ebe8f8c54e7d3c52f0a292dee/7e3e6709c93d70cfc087257df2dcd100baa12b45.jpg');
+INSERT INTO `movies_user`(`uid`, `name`, `avatar`) VALUES ('o6Z7H5T9M-YUktyJG1XKp2g5P48E', '云淡风轻', 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLByibU03NaFhGzcbRLnZFFduJOic2LdFZ0OnKVyicWYfmXYicPGALbs1fPn1QCe9vHlKKicTGBMicQYEGg/132');
